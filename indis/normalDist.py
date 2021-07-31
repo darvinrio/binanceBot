6779e814a91b91d8c4f9f4b8249ecb09f6981dde
Darvin Rio <todarvinrio@gmail.com>
2021-07-31 19:00:01 +0530

    add log Normal Dist mean and stdDev

--- indis/normalDist.py
+++ indis/normalDist.py
@@ -2,4 +2,42 @@ import numpy as np
 import pandas as pd 
 import scipy.stats as ss 
 
-def normMoments(df, l):
+def __meanColor(row, posColor='green', negColor='red'):
+    if row>=0 :
+        return posColor
+    else:
+        return negColor 
+
+def moments(df, length=20, mult=1.96):
+    logReturn = np.log(df['close']/df['open'])*100
+
+    std = logReturn.rolling(length).std(ddof = 0)
+    meanLogReturn = logReturn.rolling(length).mean()
+
+    upperBound = meanLogReturn + std
+    lowerBound = meanLogReturn - std
+
+    outDF = pd.DataFrame({
+        'openTime' : df['openTime'],
+        'logReturn' : logReturn,
+        'meanLogReturn' : meanLogReturn,
+        'upperBound' : upperBound,
+        'lowerBound' : lowerBound
+    })
+
+    return outDF
+
+def getMeanColorData(ser):
+    return ser.apply(__meanColor)
+    
+
+if __name__ == "__main__":
+    df = (pd.read_csv('../test/testData.csv')).drop('Unnamed: 0',axis=1)
+    result = moments(df,20)
+
+    # diff = result['QQEF']-result['QQES']
+    logReturn = result['logReturn']
+
+    color = getMeanColorData(logReturn)
+
+    print(color)
